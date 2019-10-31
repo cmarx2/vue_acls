d2c0b98415fa221476678665ceda06f8be807efd
Test <test@Admins-MacBook-Pro.local>
2019-10-30 19:18:03 -0600

    began app

---
+++ src/components/View.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- Your HTML goes here -->
+</template>
+
+<script>
+  /* Your JS goes here */
+</script>
+
+<style>
+  /* Your CSS goes here */
+</style>
---
+++ src/constants/dummyData.js
@@ -0,0 +1,26 @@
+export default {
+    
+        teams: {
+            vzn: {
+                vzn: [ 1, 2, 3, 4, 5],
+                att: [ 1, 2, 3, 4],
+            },
+            corp: {
+                vznb: [ 1, 2, 3, 4, 5],
+                att: [ 1, 2, 3, 4],
+                attb: [ 1, 2, 3, 4],
+            },
+        },
+        titles: {
+            sysadmin: {
+                Vzn: [ 1, 2, 3, 4, 5],
+                Att: [ 1, 2, 3, 4],
+            },
+            telecomanalyst: {
+                vznb: [ 1, 2, 3, 4, 5],
+                att: [ 1, 2, 3, 4],
+                attb: [ 1, 2, 3, 4],
+            },
+        }
+        }
+    
